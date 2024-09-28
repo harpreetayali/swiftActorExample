a95942fec4c09f4611b28b491a49064442396c06
Harpreet Singh <singhsharpreet3@gmail.com>
2024-09-28 14:22:55 +0530

    Initial Commit

---
+++ TaskManagerActor/TaskManagerActorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerActorApp.swift
+//  TaskManagerActor
+//
+//  Created by Harpreet Singh on 28/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerActorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
